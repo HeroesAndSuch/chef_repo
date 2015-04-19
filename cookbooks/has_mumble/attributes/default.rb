1f03637f6be39c0f1137bc9040a8a998ca55fc3e
Nate Walck <nate.walck@gmail.com>
2015-04-19 09:52:42 -0700

    Added mumble_server package name to see if it will help

--- cookbooks/has_mumble/attributes/default.rb
+++ cookbooks/has_mumble/attributes/default.rb
@@ -1,3 +1,4 @@
 # Attributes for the HaS Mumble Server
 default['mumble_server']['config']['welcometext'] = '<br />Welcome to HaS <b>Murmur</b>'
 default['mumble_server']['config']['registerName'] = 'HaS'
+default['mumble_server']['packages'] = ['mumble_server']
