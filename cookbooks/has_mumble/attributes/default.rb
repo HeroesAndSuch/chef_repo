1621e0359bd4b96da88d4796059fd32c7bd18868
Nate Walck <nate.walck@gmail.com>
2015-04-19 17:46:19 -0700

    Added proper mumble server user and group

--- cookbooks/has_mumble/attributes/default.rb
+++ cookbooks/has_mumble/attributes/default.rb
@@ -2,3 +2,5 @@
 default['mumble_server']['config']['welcometext'] = '<br />Welcome to HaS <b>Murmur</b>'
 default['mumble_server']['config']['registerName'] = 'HaS'
 default['mumble_server']['packages'] = ['mumble-server']
+default['mumble_server']['user'] = 'mumble-server'
+default['mumble_server']['group'] = 'mumble-server'
