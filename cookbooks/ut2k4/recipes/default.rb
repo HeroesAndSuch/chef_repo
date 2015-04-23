d7cc6717c913f34f42b4edd15d8c88db0b212944
Nate Walck <n8@dropbox.com>
2015-04-23 12:47:21 -0700

    Added some missing bits

--- cookbooks/ut2k4/recipes/default.rb
+++ cookbooks/ut2k4/recipes/default.rb
@@ -6,6 +6,7 @@
 
 # For Ubuntu 64-bit
 required_packages = ['mailutils', 'postfix', 'unzip', 'tmux', 'libstdc++5:i386']
+required_dirs = ['/home/ut2k4server', '/usr/local/bin/serverfiles']
 
 required_packages.each do |pkg_name|
   package pkg_name do
@@ -17,6 +18,17 @@ user "ut2k4server" do
   action :create
   system true
   home "/home/ut2k4server"
+  shell "/bin/bash"
+end
+
+
+required_dirs.each do |dir_name|
+  directory dir_name do
+    owner "ut2k4server"
+    group "ut2k4server"
+    mode '0755'
+    action :create
+  end
 end
 
 remote_file "/usr/local/bin/ut2k4server" do
